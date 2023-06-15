faaade2763a8dd8c26853c052ea5c2f136289a38
DarkTuga5 <136704089+DarkTuga5@users.noreply.github.com>
2023-06-15 13:52:51 +0100

    script.js

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
-document.getElementById("booking-form").addEventListener("submit", function(event) {
+function submitForm(event) {
   event.preventDefault();
-  var date = document.getElementById("date").value;
-  
-  // Aqui você pode adicionar a lógica para processar e armazenar as marcações de almoço
 
-  // Exemplo: exibir uma mensagem de sucesso
-  document.getElementById("booking-form").reset();
-  document.getElementById("booking-success").classList.remove("hidden");
+  // Obtenha os valores selecionados do menu e as informações do cliente
+  var selectedMenu = document.getElementById("menu").value;
+  var customerName = document.getElementById("name").value;
+  var customerEmail = document.getElementById("email").value;
 
-// Obtenha os valores selecionados do menu e as informações do cliente
-var selectedMenu = document.getElementById("menu").value;
-var customerName = document.getElementById("name").value;
-var customerEmail = document.getElementById("email").value;
-
-// Preencha os elementos na página de confirmação
-document.getElementById("selectedMenu").textContent = selectedMenu;
-document.getElementById("customerName").textContent = "Nome: " + customerName;
-document.getElementById("customerEmail").textContent = "Email: " + customerEmail;
+  // Preencha os elementos na página de confirmação
+  document.getElementById("selectedMenu").textContent = selectedMenu;
+  document.getElementById("customerName").textContent = "Nome: " + customerName;
+  document.getElementById("customerEmail").textContent = "Email: "
