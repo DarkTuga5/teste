546f0967423d6c46c0b51cb63134be779a373860
DarkTuga5 <136704089+DarkTuga5@users.noreply.github.com>
2023-06-15 13:39:57 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("booking-form").addEventListener("submit", function(event) {
+  event.preventDefault();
+  var date = document.getElementById("date").value;
+  var time = document.getElementById("time").value;
+  
+  // Aqui você pode adicionar a lógica para processar e armazenar as marcações de almoço
+
+  // Exemplo: exibir uma mensagem de sucesso
+  document.getElementById("booking-form").reset();
+  document.getElementById("booking-success").classList.remove("hidden");
+});
