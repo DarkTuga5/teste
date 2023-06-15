0bbd94b5605fffa0eda54775c4bc48fc2fd2de24
DarkTuga5 <136704089+DarkTuga5@users.noreply.github.com>
2023-06-15 13:48:05 +0100

    script.js

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ document.getElementById("booking-form").addEventListener("submit", function(even
   document.getElementById("booking-form").reset();
   document.getElementById("booking-success").classList.remove("hidden");
 });
+
+// Obtenha os valores selecionados do menu e as informações do cliente
+var selectedMenu = document.getElementById("menu").value;
+var customerName = document.getElementById("name").value;
+var customerEmail = document.getElementById("email").value;
+
+// Preencha os elementos na página de confirmação
+document.getElementById("selectedMenu").textContent = selectedMenu;
+document.getElementById("customerName").textContent = "Nome: " + customerName;
+document.getElementById("customerEmail").textContent = "Email: " + customerEmail;
